e78ee7f1804cbd8804a525ff63f6490435fec353
André Lacerda <a.de.sousa.lacerda@avanade.com>
2024-11-05 16:19:24 -0300

    implementacao backend

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.getElementById('cadastroForm').addEventListener('submit', async functio
   const telefone = document.getElementById('telefone').value;
   const email = document.getElementById('email').value;
   const senha = document.getElementById('senha').value;
+  const form = document.getElementById('cadastroForm');
 
   const response = await fetch('http://localhost:3000/api/barbearia', {
       method: 'POST',
@@ -17,13 +18,21 @@ document.getElementById('cadastroForm').addEventListener('submit', async functio
       body: JSON.stringify({ nome, telefone, email, sobrenome, senha }),
   });
 
-  const messageElement = document.getElementById('message');
+  const messageElement = document.getElementById('alerta');
   if (response.ok) {
-      messageElement.textContent = 'Barbeiro cadastrado com sucesso!';
-      messageElement.className = 'alert alert-success';
+    messageElement.textContent = 'Usuário cadastrado com sucesso!';
+    messageElement.className = 'alert alert-success';
+    messageElement.style.display = 'block'; 
+    form.reset();
   } else {
-      messageElement.textContent = 'Erro ao cadastrar barbeiro.';
-      messageElement.className = 'alert alert-danger';
+    messageElement.textContent = 'Erro ao cadastrar usuário.';
+    messageElement.className = 'alert alert-danger';
+    messageElement.style.display = 'block'; 
   }
+
+  setTimeout(() => {
+    messageElement.style.display = 'none'; 
+}, 2000);
+
 });
 
